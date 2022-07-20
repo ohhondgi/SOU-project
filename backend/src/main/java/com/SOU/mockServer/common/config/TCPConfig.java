4fc9ab59d20c96e022c5ba6002ffa8188112632c
OhHongGi <5ghdrl@gmail.com>
2022-07-20 15:33:27 +0900

    chore: Change Tcp Server Structure

--- backend/src/main/java/com/SOU/mockServer/common/config/TCPConfig.java
+++ backend/src/main/java/com/SOU/mockServer/common/config/TCPConfig.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.Router;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.ip.tcp.TcpInboundGateway;
 import org.springframework.integration.ip.tcp.connection.AbstractServerConnectionFactory;
 import org.springframework.integration.ip.tcp.connection.TcpNetServerConnectionFactory;
+import org.springframework.integration.router.PayloadTypeRouter;
 import org.springframework.messaging.MessageChannel;
 
 @EnableIntegration
@@ -31,42 +33,6 @@ public class TCPConfig {
     @Value("${tcp.server.host}")
     private String host;
 
-//    @Bean
-//    public AbstractClientConnectionFactory clientConnectionFactory() {
-//
-//        TcpNetClientConnectionFactory clientCF = new TcpNetClientConnectionFactory(this.host,
-//            this.port);
-//        clientCF.setSerializer(
-//            new OnlineMessageSerializer(maxMessageSize, lengthHeaderSize, new BytesConverter(),
-//                treatTimeoutAsEndOfMessage));
-//        clientCF.setDeserializer(
-//            new OnlineMessageDeserializer(maxMessageSize, lengthHeaderSize, new BytesConverter(),
-//                treatTimeoutAsEndOfMessage));
-//        // thread가 message를 보낼 때 마다 새로운 연결을 생성하는 설정(singleUse)
-//        // 높은 처리량을 제공 -> 매번 새로운 연결 생성에 따른 오버헤드 증가
-////        clientCF.setSingleUse(true);
-//        // connection 당 reply 대기시간으로, default는 10000(10 sec)
-////        clientCF.setSoTimeout(1000);
-//        return clientCF;
-//    }
-
-//    @Bean
-//    public ThreadAffinityClientConnectionFactory tacf() {
-//        return new ThreadAffinityClientConnectionFactory(clientConnectionFactory());
-//    }
-
-    // clientConnectionFactory를 사용하여 channel을 통해 message를 전송함,
-    // reply message를 수신하면,
-//    @ServiceActivator(inputChannel = "inboundChannel", outputChannel = "replyChannel")
-//    @Bean
-//    public TcpOutboundGateway tcpOutGate() {
-//        TcpOutboundGateway outGate = new TcpOutboundGateway();
-//        outGate.setReplyChannel(replyChannel());
-//        outGate.setConnectionFactory(clientConnectionFactory());
-//        return outGate;
-//    }
-
-
     @Bean
     public AbstractServerConnectionFactory serverConnectionFactory() {
         TcpNetServerConnectionFactory serverCF = new TcpNetServerConnectionFactory(this.port);
@@ -76,7 +42,6 @@ public class TCPConfig {
         serverCF.setSerializer(
             new OnlineMessageSerializer(maxMessageSize, lengthHeaderSize, new BytesConverter(),
                 treatTimeoutAsEndOfMessage));
-        // message를 수신하고 응답할 때마다 새로운 연결을 생성하는 설정(singleUse=ture)
         serverCF.setSingleUse(true);
         serverCF.setSoTcpNoDelay(true);
         serverCF.setSoKeepAlive(true);
@@ -84,37 +49,38 @@ public class TCPConfig {
     }
 
 
-    /*
-    server client factory 사용하여 message 수신
-    1. 전달받는 payload로 message를 구성한 후에, requestChannel에 message를 전송
-    2. response message를 수신하면, response message로부터 payload를 connection으로 전송
-     */
     @Bean
-    public TcpInboundGateway tcpInGate() {
+    public TcpInboundGateway tcpInboundGateway() {
         TcpInboundGateway inGate = new TcpInboundGateway();
         inGate.setConnectionFactory(serverConnectionFactory());
         inGate.setRequestChannel(inboundChannel());
-        inGate.setReplyChannel(replyChannel());
+        inGate.setReplyChannel(outboundChannel());
+        inGate.setErrorChannel(errorChannel());
         return inGate;
+    }
+
+    @Router(inputChannel = "inboundChannel")
+    @Bean
+    public PayloadTypeRouter router() {
+        PayloadTypeRouter router = new PayloadTypeRouter();
+        // 각 전문 channel 로 전송
+        router.setChannelMapping(CommonFieldMessage.class.getName(), "CommonMessageChannel");
+        router.setChannelMapping(NotificationIndividualWithdrawalMessage.class.getName(),
+            "NotificationIndividualWithdrawalMessageChannel");
+        return router;
     }
 
     @Bean
     public MessageChannel inboundChannel() {
-        DirectChannel channel = new DirectChannel();
-        // 특정 type에 대한 data만 허용
-//        channel.setDatatypes(CommonFieldMessage.class,
-//            NotificationIndividualWithdrawalMessage.class);
-        return channel;
-    }
-
-//    @Bean MessageChannel outboundChannel() {
-//        return new DirectChannel();
-//    }
-
-    @Bean
-    public MessageChannel replyChannel() {
         return new DirectChannel();
     }
 
+    @Bean
+    public MessageChannel outboundChannel() { return new DirectChannel(); }
+
+    @Bean
+    public MessageChannel errorChannel() {
+        return new DirectChannel();
+    }
 
 }
--- backend/src/main/java/com/SOU/mockServer/external/controller/TcpController.java
+++ backend/src/main/java/com/SOU/mockServer/external/controller/TcpController.java
@@ -1,5 +1,6 @@
 package com.SOU.mockServer.external.controller;
 
+import com.SOU.mockServer.common.message.Message;
 import com.SOU.mockServer.external.message.account.NotificationIndividualWithdrawalMessage;
 import com.SOU.mockServer.external.message.common.CommonFieldMessage;
 import com.SOU.mockServer.external.service.TcpService;
@@ -10,26 +11,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// inbound 요청을 서비스 계층 호출로 변환, 서비스 계층 반환 값을 아웃바운드 응답으로 변환하는 하나의 layer
-// pipe & filter architecture 의 filter에 해당
-// test flow에서 사용할 수 있는 여러 POJO(plain old java object) method 제공가능 (inputChannel의 data type에 따라 분리)
+// inbound 요청을 서비스 계층 호출로 변환, 서비스 계층의 처리 후 반환 값을 아웃바운드 응답으로 변환하는 하나의 layer
+// pipe & filter architecture 의 filter 에 해당
+// test flow에서 사용할 수 있는 여러 POJO(plain old java object) method 제공가능
 @MessageEndpoint
 @Component
 public class TcpController {
     private static final Logger LOGGER = LoggerFactory.getLogger(TcpController.class);
 
-    // inputChannel은 TCPConfig에서 등록한 bean이름과 동일시하기
-    @ServiceActivator(inputChannel = "inboundChannel")
+    @ServiceActivator(inputChannel = "CommonMessageChannel", outputChannel = "outboundChannel")
     public CommonFieldMessage handleCommonFieldMessage(CommonFieldMessage msg) {
+        LOGGER.debug("Arrive CommonMessage at Controller");
+        // 추후 Common message logic에 대한 처리 가능
         return msg;
     }
 
-    @ServiceActivator(inputChannel = "inboundChannel")
+    @ServiceActivator(inputChannel = "NotificationIndividualWithdrawalMessageChannel", outputChannel = "outboundChannel")
     public NotificationIndividualWithdrawalMessage handleNotificationIndividualWithdrawalMessage(NotificationIndividualWithdrawalMessage msg) {
+        LOGGER.debug("Arrive NotificationIndividualWithdrawalMessage at Controller");
+        // 추후 NotificationIndividualWithdrawalMessage logic에 대한 처리 가능
         return msg;
     }
 
